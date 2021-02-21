7e54888c69584f6f200e8b4e9f1ef4c2913f5276
brthema072 <brunomarthe@hotmail.com>
2021-02-21 15:14:16 -0300

    add method showCustomNotification in messaging.service

--- src/app/service/messaging.service.ts
+++ src/app/service/messaging.service.ts
@@ -34,7 +34,26 @@ export class MessagingService {
       (payload: any) => {
         console.log("new message received. ", payload.notification.body);
         this.currentMessage.next(payload.notification);
+        this.showCustomNotification(payload)
       })
+  }
+
+  showCustomNotification(payload: any){
+    let notify_data = payload["notification"];
+    let title = notify_data["title"];
+    let options = {
+      body: notify_data["body"],
+      icon: "./assets/navbar-circlu-animation.png",
+      badge: "./assets/navbar-circlu-animation.png",
+      image: "./assets/navbar-circlu-animation.png"
+    }
+
+    let notify: Notification = new Notification(title, options)
+
+    notify.onclick = event => {
+      event.preventDefault();
+      window.open("https://www.google.com", "_blank");
+    }
   }
 
 }
