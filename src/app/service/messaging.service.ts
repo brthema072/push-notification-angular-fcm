9c778e34ce7892ccad5876df150469cf0b27c5b1
brthema072 <brunomarthe@hotmail.com>
2021-02-21 15:17:10 -0300

    remove import observable not using

--- src/app/service/messaging.service.ts
+++ src/app/service/messaging.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/messaging';
-import { BehaviorSubject, Observable } from 'rxjs'
+import { BehaviorSubject } from 'rxjs'
 
 
 @Injectable({
